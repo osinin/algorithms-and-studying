7e7c15333cdd021081bdfb8d38be02dc350d828e
dmitriyosinin <osinin.dm@yandex.ru>
2024-01-08 12:34:26 +0530

    karpov courses testing

---
+++ data_structures/linked_list.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, data):
+        self.value = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            self.tail = new_node
+            self.head = new_node
+
+    def remove_at_beginning(self):
+        self.head = self.head.next
+
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.value, end=" -> ")
+            current_node = current_node.next
+        print("None")  # To indicate the end of the lis
+
+
+ll = LinkedList()
+ll.insert_at_beginning("done")
+#ll.insert_at_beginning("mix")
+#ll.insert_at_beginning("prepare")
+ll.print_list()
